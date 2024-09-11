402e10dc0ce2b60e3f9a804310a5ee3ab94c7e66
mustakin04 <mustakinhasan37@gmail.com>
2024-09-12 02:44:35 +0600

    hw

---
+++ array.js
@@ -0,0 +1,12 @@
+let number = [20, 30, 40, 50];
+console.log(number);
+console.log("Length " + number.length);
+console.log("Three number of index " + number[3]);
+number.push(60);
+console.log("Push="+number);
+number.pop();
+console.log("Pop=" +number);
+number.shift(10);
+console.log("Shift="+number);
+number.unshift(10);
+console.log("Unshift="+number);
