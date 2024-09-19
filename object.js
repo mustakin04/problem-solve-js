54c91ab5136fbca37f3e4a4012a489d531a606e6
mustakin04 <mustakinhasan37@gmail.com>
2024-09-19 14:16:43 +0600

    object create for loop

---
+++ object.js
@@ -0,0 +1,11 @@
+let person={
+    name:'Md Mustakin Hassan',
+    age:24,
+    Phone:'01714977614',
+    relationship:'single',
+}
+
+const dako=person.name;
+console.log(dako);
+person.Phone='01311605545';
+console.log(person);
---
+++ object_loop.js
@@ -0,0 +1,9 @@
+let student={
+    name:"tamim",
+    age:15,
+    bio: "she is a beautifull girl",
+}
+for(let cont in student){
+    console.log(cont);
+    console.log(student[cont]);
+}
