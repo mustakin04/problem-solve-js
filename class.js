2c71dc02c16bbe08d8c5e08ebfbcb6d7370e7249
mustakin04 <mustakinhasan37@gmail.com>
2024-10-23 04:47:16 +0600

    ok

---
+++ class.js
@@ -0,0 +1,14 @@
+class Myclass {
+    constructor(name, year) {
+        this.name = name;
+        this.year = year;
+    }
+    play(){
+        console.log("sdlfaslj")
+    }
+}
+// const ok=new Myclass.play()
+const class1 = new Myclass("kuyasha", 24);
+console.log(class1.name)
+class1.play()
+// Myclass.play()
---
+++ ternary.js
@@ -0,0 +1,6 @@
+let num=9;
+
+const result=(num>=10)? "true"
+             :(num<=5)? "five":"six to nine";
+
+             console.log(result)
